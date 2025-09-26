8064150bb63de8115e600d17f2cccc131697858e
Shane Lin <skyshanelin@gmail.com>
2025-09-26 23:22:34 +0800

    test

--- multiplication_table.cpp
+++ multiplication_table.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
-#include<string>
 #include<cmath>
 using namespace std;
 
 int main() 
 {
-    cout << "114611008林宥翔" << endl;
+    cout << "114611008林宥翔" << endl ;
+    int num , m , n;
     char word[99];
-    while(3388){
-      cout << "Give me a interger number among 6 ~ 19 , input q if you want to quit." << endl;
-      cin >> word ;
-      int m , n;
-       
-      if(word ==  "Q" ){
-        cout << "Bye bye" << endl ; 
-        return 0;
-      }
-      int num = atoi(word);
-      if(num >= 6 && num <=19){
-        for(m = 1 ; m <= num ; m++ ){
-          cout << endl ;
-          for(n = 1; n <= num ; n++){
-            cout << n*m << " ";
-
+    cout << "Give me a interger number among 6 ~ 19 , input Q if you want to quit." << endl;
+    cin >>  word ;
+    if ( word[0] == 'Q' ){
+      cout << "Bye bye" ;
+      return 0;
+    }
+    num = atoi(word);
+    if(num >= 6 && num <=19){
+      for(m = 1 ; m <= num ; m++ ){
+        cout << endl;
+        for(n = 1; n <= num ; n++){
+            cout << m*n << " " ;
+            if (m*n < 10){
+              cout << "  ";
+            }
+            if(m * n < 100 && m*n >= 10){
+              cout << " ";
+            }
           }
         }
-        cout << endl ;
-
       }else{
-        cout << "your selected number is illegal" ;
+        cout << "your input is illegal" <<endl;
       }
       return 0;
     }
     
-}
